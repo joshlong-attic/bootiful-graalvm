f37dafc3e2d1c806caf2f3de6b419c96b204247a
Josh Long <josh@joshlong.com>
2021-03-12 20:02:37 -0800

    build

--- actuator-hibernate-servlets/build.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-mvn clean spring-boot:build-image
-docker run -p 8080:8080 -e SERVER_PORT=8080 docker.io/library/bootiful:0.0.1-SNAPSHOT
--- reactive/build.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-mvn clean spring-boot:build-image
-docker run -p 8080:8080 -e SERVER_PORT=8080 docker.io/library/reactive:0.0.1-SNAPSHOT
