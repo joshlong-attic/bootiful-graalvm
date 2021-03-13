1a827da5c53469bd41947f3847d9a3716a676f2d
Josh Long <josh@joshlong.com>
2021-03-12 18:15:51 -0800

    up

--- reactive/build.sh
+++ reactive/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 mvn clean spring-boot:build-image
-docker run -p 8080:8080 -e SERVER_PORT=8080 docker.io/library/bootiful:0.0.1-SNAPSHOT
+docker run -p 8080:8080 -e SERVER_PORT=8080 docker.io/library/demo:0.0.1-SNAPSHOT
